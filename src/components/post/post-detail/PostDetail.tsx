d4ed994f7960e6b150f9b9eda54b5db857cf7e37
raiza r <rahmanraiza@gmail.com>
2024-12-21 15:35:56 +0700

    fix(seo): improve seo on postdetail

--- src/components/post/post-detail/PostDetail.tsx
+++ src/components/post/post-detail/PostDetail.tsx
@@ -3,12 +3,13 @@ import { useParams } from 'react-router-dom';
 import { getPostDetail } from '../../../services/getPost';
 import { PostDetailProps, PostSEOProps } from '../../../types/post';
 import PacmanLoading from '../../shared/loading/PacmanLoading';
+import { Helmet } from 'react-helmet';
 
 import styles from './PostDetail.module.css';
 
 export default function PostDetail() {
   const [postDetails, setPostDetails] = useState<PostDetailProps | null>(null);
-  const [postSEO, setPostSEO] = useState<PostSEOProps | null>(null);
+  const [postSEO, setPostSEO] = useState<PostSEOProps | null>(null); // Separate state for postSEO
   const { id } = useParams<{ id: string }>();
 
   useEffect(() => {
@@ -27,33 +28,6 @@ export default function PostDetail() {
     fetchPostDetail();
   }, [id]);
 
-  useEffect(() => {
-    document.title = postSEO?.headline || 'Default Title';
-    document
-      .querySelector('meta[name="description"]')
-      ?.setAttribute('content', postSEO?.description || '');
-
-    document
-      .querySelector('meta[property="og:title"]')
-      ?.setAttribute('content', postSEO?.headline || '');
-    document
-      .querySelector('meta[property="og:description"]')
-      ?.setAttribute('content', postSEO?.description || '');
-    document
-      .querySelector('meta[property="og:image"]')
-      ?.setAttribute('content', postSEO?.image || '');
-
-    document
-      .querySelector('meta[name="twitter:title"]')
-      ?.setAttribute('content', postSEO?.headline || '');
-    document
-      .querySelector('meta[name="twitter:description"]')
-      ?.setAttribute('content', postSEO?.description || '');
-    document
-      .querySelector('meta[name="twitter:image"]')
-      ?.setAttribute('content', postSEO?.image || '');
-  }, [postSEO]);
-
   if (!postDetails || !postSEO) {
     return (
       <div className={styles.fallbackContainer}>
@@ -63,25 +37,38 @@ export default function PostDetail() {
   }
 
   return (
-    <div className={styles.container}>
-      <div
-        dangerouslySetInnerHTML={{ __html: postDetails.content }}
-        className={styles.content}
-      />
+    <>
+      <Helmet>
+        <title>{postSEO.headline || 'Default Title'}</title>
+        <meta name="description" content={postSEO.description} />
+        <meta property="og:title" content={postSEO.headline} />
+        <meta property="og:description" content={postSEO.description} />
+        <meta property="og:image" content={postSEO.image || ''} />
+        <meta property="og:url" content={postSEO.url} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={postSEO.headline} />
+        <meta name="twitter:description" content={postSEO.description} />
+        <meta name="twitter:image" content={postSEO.image || ''} />
+      </Helmet>
 
-      <div className={styles.divider} />
-
-      <div className={styles.infoContainer}>
-        <p className={styles.date}>
-          {`Posted on `}
-          {new Date(postDetails.created_at).toLocaleDateString('en-US', {
-            day: '2-digit',
-            month: 'short',
-            year: 'numeric',
-          })}
-        </p>
-        <p className={styles.posted}>- Raiza</p>
+      <div className={styles.container}>
+        <div
+          dangerouslySetInnerHTML={{ __html: postDetails.content }}
+          className={styles.content}
+        />
+        <div className={styles.divider} />
+        <div className={styles.infoContainer}>
+          <p className={styles.date}>
+            {`Posted on `}
+            {new Date(postDetails.created_at).toLocaleDateString('en-US', {
+              day: '2-digit',
+              month: 'short',
+              year: 'numeric',
+            })}
+          </p>
+          <p className={styles.posted}>- Raiza</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
